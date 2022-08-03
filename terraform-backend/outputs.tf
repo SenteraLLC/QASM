bb3fa1b5ba9b80de175ffa871e82af00cc98eaf3
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-08-03 15:32:43 -0500

    auto populate api endpoints

--- terraform-backend/outputs.tf
+++ terraform-backend/outputs.tf
@@ -3,3 +3,13 @@ output "ecs_lambda_api_gateway_url" {
 
 	value = aws_apigatewayv2_stage.lambda_api_stage.invoke_url
 }
+
+locals {
+	env_suffix = terraform.workspace == "prod" ? "production" : "development"
+}
+
+resource "local_file" "dev_url" {
+	content = "REACT_APP_API_URL=${aws_apigatewayv2_stage.lambda_api_stage.invoke_url}/"
+	
+	filename = "../react-frontend/.env.${local.env_suffix}"
+}
