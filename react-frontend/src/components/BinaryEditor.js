daf5519ba3ec58f5b80e88d2eae5a90a35857f5b
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2022-08-15 16:02:23 -0500

    Add binary editor page

---
+++ react-frontend/src/components/BinaryEditor.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "../css/BinaryEditor.css";
+
+class BinaryEditor extends Component {
+    render() {
+        return (
+            <div className="BinaryEditor">
+                BinaryEditor
+            </div>
+        )
+    }
+}
+
+export default BinaryEditor
