bdeb38506b07dd6504f604d839ebf33bb4b83732
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2022-08-16 14:34:50 -0500

    Add close method to BinaryEditor

--- react-frontend/src/components/BinaryEditor.js
+++ react-frontend/src/components/BinaryEditor.js
@@ -4,7 +4,7 @@ import "../css/BinaryEditor.css";
 const { Image } = require("image-js");
 
 class BinaryEditor extends Component {
-    updateCanvas() {
+    async updateCanvas() {
         // Grab the input element
         let input = document.querySelector("#image_input");
 
@@ -13,14 +13,38 @@ class BinaryEditor extends Component {
 
         // Update the original-binary img with the new src
         document.querySelector("#original-binary").src = original_binary_src;
-        
+
+        const image = await Image.load(original_binary_src);
+
+        const binary = image.grey().mask();
+
+
+        document.querySelector("#output-binary").src = binary.toDataURL();
+    }
+
+    async close() {
+        let binary = await Image.load(document.querySelector("#output-binary").src);
+
+        binary = binary.grey().mask();
+
+        let new_binary = binary.close([[1,1,1],[1,1,1],[1,1,1]]);
+
+        console.log(binary)
+
+        document.querySelector("#output-binary").src = new_binary.toDataURL();
     }
 
     render() {
         return (
             <div className="BinaryEditor">
-                <input type="file" accept=".jpeg,.JPEG,.png,.PNG" id="image_input" onChange={this.updateCanvas} />
-                <img id="original-binary" alt="Original Binary"></img>
+                <button onClick={this.close}>
+                    Close
+                </button>
+                <input type="file" accept=".jpeg,.JPEG,.png,.PNG,.jpg,.JPG" id="image_input" onChange={this.updateCanvas} />
+                <div>
+                    <img id="original-binary" alt="Original Binary" />
+                    <img id="output-binary" alt="Output"/>
+                </div>
             </div>
         )
     }
