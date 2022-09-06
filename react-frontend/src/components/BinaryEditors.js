b30585eb6d7b7eb40d875c9249c02d34e7b1fb27
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2022-09-06 13:26:24 -0500

    Use BinaryEditors component to render diffrent binary editors based on mode

--- react-frontend/src/components/BinaryEditors.js
+++ react-frontend/src/components/BinaryEditors.js
@@ -1,8 +1,26 @@
 import { Component } from "react";
 import SingleBinaryEditor from "./SingleBinaryEditor";
 
+/**
+ * Returns diffrent Binary Editors based on the whichever mode is passed in.
+ * SingleBinaryEditor is the default.
+ */
 class BinaryEditors extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.mode = props.mode;
+    }
+
     render() {
-        return
+        switch (this.mode) {
+            case "placeholder":
+                return
+            case "single":
+            default:
+                return <SingleBinaryEditor {...this.props}/>
+        }
     }
 }
+
+export default BinaryEditors;
