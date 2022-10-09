785b1331d76bdc5e57b377da97d4d0f263420e77
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2022-10-09 14:16:04 -0500

    Add basic styling and structure to Dropdown

--- react-frontend/src/components/Dropdown.js
+++ react-frontend/src/components/Dropdown.js
@@ -3,6 +3,32 @@ import "../css/Dropdown.css";
 
 class Dropdown extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.items        = props.items;
+        this.callback     = props.callback;
+        this.style        = props.style;
+        this.display_text = props.display_text;
+
+        if (this.display_text === undefined || null) {
+            this.display_text = "▶"
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="Dropdown">
+                <button className="toggle-display">
+                    {this.display_text}
+                </button>
+                <div className="dropdown-content">
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Dropdown
