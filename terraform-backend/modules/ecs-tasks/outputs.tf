23fea0926aab34711c5174df8481c225f1297f52
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-10-11 20:03:49 -0500

    Port over terraform ecs structure from mfstand

---
+++ terraform-backend/modules/ecs-tasks/outputs.tf
@@ -0,0 +1,13 @@
+output "ecs_cluster_name" { 
+    value = var.ecs_cluster_name
+}
+
+output "task_name" {
+    value = "${terraform.workspace}-qasm-${var.base_name}-task"
+}
+output "task_def_id" {
+    value = aws_ecs_task_definition.task_definition.id
+}
+output "task_revision" {
+    value = aws_ecs_task_definition.task_definition.revision
+}
