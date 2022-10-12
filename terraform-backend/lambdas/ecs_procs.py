259dd545bd8bb7b11e173c9eacc21d6538506f38
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-10-12 13:26:21 -0500

    Binary dir creation

--- terraform-backend/lambdas/ecs_procs.py
+++ terraform-backend/lambdas/ecs_procs.py
@@ -3,5 +3,5 @@ from utils.ecs_utils import ecs_lambda_handler_block
 
 
 def ecs_binary_directory(event, context):
-    """Exg segmentation ECS lambda handler."""
+    """Binary directory ECS lambda handler."""
     return ecs_lambda_handler_block(event, "BINARY DIRECTORY")
