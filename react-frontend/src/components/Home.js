dee1c7405dd7d1bf894cea35bf800ac9dbc626a1
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-10-24 13:54:26 -0500

    Ulabel kinda works

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -1,13 +1,21 @@
 import { Component } from 'react';
 import icon from "../../public/icon.png";
+import Ulabel from "./Ulabel.js";
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        // Initialize props
+        this.QASM = props.QASM;
+    }
 
     render() {
         return (
             <div className="Home">
                 <h2>QASM Home</h2>
                 <img src={icon} alt="icon"/>
+                <Ulabel QASM={this.QASM}></Ulabel>
             </div>
         )
     }
