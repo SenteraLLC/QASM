b45fddfb24fa47cb576f976b3a0f220347f86edc
Carter Solberg <65569256+csolbs24@users.noreply.github.com>
2022-11-02 16:48:09 -0500

    Fix not being able to navigate between same type of component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -30,35 +30,63 @@ class App extends Component {
     // this.component_keys = Object.keys(this.components);
     this.location       = window.location.href.split("/").slice(-1)[0] // Just page name
 
-    // Create object to keep track of number of diffrent components
+    // Create object to keep track of number of different components
     let component_counter = {};
     
-    for (let component_idx in this.components) {
-      // Grab the props passed to each component
-      let props = this.components[component_idx]
+    for (let component of this.components) {
+      // Add QASM to each component
+      component.QASM = this.QASM;
 
-      // Add QASM object to all component props
-      props.QASM = this.QASM;
+      // If the component is home the path will always be /
+      if (component.component === "home") {
+        component.path = "/"
+      }
+      // If its the first instance of a component the component_counter will be undefined for that components
+      else if (component_counter[component.component] === undefined) {
+        // Set the component_counter to 1 for that component
+        component_counter[component.component] = 1;
 
-      if (component_counter[props.component] === undefined) {
-        // Set the component counter for this component to 1
-        component_counter[props.component] = 1;
-
-        // Then set the key
-        props.component_key = props.component;
+        // Set the path to the component name
+        component.path = component.component;
       }
       else {
-        // If the component counter for this component is already a number, then add 1 to it
-        component_counter[props.component] += 1;
+        // Add 1 to the component_counter
+        component_counter[component.component] += 1;
 
-        // This isn't the first component of this type, so its id will be the component name plus which number component it is
-        props.component_key = props.component + component_counter[props.component];
+        // Set the path to be the component name + the number of that component so far.
+        component.path = component.component + component_counter[component.component];
       }
 
-      console.log(props)
+
+
+
+
+
+      // // Grab the props passed to each component
+      // let props = this.components[component_idx]
+
+      // // Add QASM object to all component props
+      // props.QASM = this.QASM;
+
+      // if (component_counter[props.component] === undefined) {
+      //   // Set the component counter for this component to 1
+      //   component_counter[props.component] = 1;
+
+      //   // Then set the key
+      //   props.component_key = props.component;
+      // }
+      // else {
+      //   // If the component counter for this component is already a number, then add 1 to it
+      //   component_counter[props.component] += 1;
+
+      //   // This isn't the first component of this type, so its id will be the component name plus which number component it is
+      //   props.component_key = props.component + component_counter[props.component];
+      // }
+
+      // console.log(props)
       // Build component list
       this.componentList.push(
-        COMPONENT_KEYS[props.component](props)
+        COMPONENT_KEYS[component.component](component)
       )
     }
 
@@ -73,6 +101,7 @@ class App extends Component {
   }
   
   render() {
+    console.log(this.components)
     return (
       <HashRouter>
       <div className="App">
@@ -85,11 +114,8 @@ class App extends Component {
             {this.components.map(component => (
               <Link 
                 className="Link"
-                to={component.component === "home" 
-                  ? "/" 
-                  : component.component_key
-                }
-                key={component.component_key}> 
+                to={component.path}
+                key={component.path}> 
                 <h2>
                   {component.display_name === undefined 
                   ? component.component
@@ -102,10 +128,7 @@ class App extends Component {
         <Routes>
           {this.componentList.map((component, idx) => (
             <Route 
-              path={this.components[idx].component === "home"
-                ? "/"
-                : this.components[idx].component.component_key
-              }
+              path={component.props.path}
               element={component}
               key={idx}/>
           ))}
