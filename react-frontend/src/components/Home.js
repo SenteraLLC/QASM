db09cb9efa786d11ce6ace7d93fc17a7999b2e18
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-11-30 13:00:22 -0600

    Hack to make s3Browser work with memoryrouter

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component} from 'react';
 import icon from "../../public/icon.png";
 
 class Home extends Component {
@@ -7,6 +7,24 @@ class Home extends Component {
 
         // Initialize props
         this.QASM = props.QASM;
+
+        // Bind functions
+        this.forwardToS3Browser = this.forwardToS3Browser.bind(this);
+    }
+
+    forwardToS3Browser() {
+        // Hack to get to s3 browser using memory router
+        if (window.S3_BROWSER_MODE !== undefined) {
+            let link = document.getElementById("s3browser-link");
+            link.click();
+        }
+    }
+
+    componentDidMount() {
+        // Runs on page load. The timeout makes it actually work for some reason
+        setTimeout(() => {
+            this.forwardToS3Browser();
+        }, 0)
     }
 
     render() {
