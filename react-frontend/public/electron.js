3079b0d777a25dcbb59cb17042da4db9d0286bb6
Joshua Dean <deanx252@umn.edu>
2022-12-15 14:23:31 -0600

    Adjust clip script.

--- react-frontend/public/electron.js
+++ react-frontend/public/electron.js
@@ -22,7 +22,7 @@ function createWindow() {
   win.loadURL(
     isDev
       ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      : `file://${path.join(process.cwd(), "../build/index.html")}`
   );
 
   // Open the DevTools.
