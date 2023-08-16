51dc367e7d2f72295d70fa52355498f59234f8db
TrevorBurgoyne <trevorburgoyne@gmail.com>
2023-08-16 16:42:08 -0500

    Only intercept s3 protocol if defined in config

--- react-frontend/public/electron.js
+++ react-frontend/public/electron.js
@@ -1,49 +1,62 @@
 // Electron desktop application
+const fs = require('fs')
 const path = require("path");
 const electron_utils = require("./electron_utils.js");
 const { s3_browser_modes } = require("../src/QASM/constants.js");
-const { app, BrowserWindow, dialog } = require("electron");
+const { app, BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
 const s3_protocol = "s3" // followed by '://', e.g. 's3://'
-
-// https://www.electronjs.org/docs/latest/tutorial/launch-app-from-url-in-another-app
-if (process.defaultApp) {
-  if (process.argv.length >= 2) {
-    app.setAsDefaultProtocolClient(s3_protocol, process.execPath, [path.resolve(process.argv[1])])
-  }
-} else {
-  app.setAsDefaultProtocolClient(s3_protocol)
-}
-
-const gotTheLock = app.requestSingleInstanceLock()
 let mainWindow
 
-function openDeepLink(mainWindow, deep_link) {
-  // remove s3 and the '://'
-  let s3_path = deep_link.slice(s3_protocol.length + "://".length); 
-  // get the first part of the path, which is the bucket name
-  let bucket_name = s3_path.split("/")[0];
-  // get the rest of the path, which is the folder name
-  let start_folder = s3_path.slice(bucket_name.length + 1);
-
-  // open an s3 browser window
-  mainWindow.webContents.executeJavaScript(`try{let popup}catch{}; popup = window.open(window.location.href, "S3 Browser"); popup.window.S3_BROWSER_MODE = "${s3_browser_modes.DEEP_LINK}"; popup.window.START_FOLDER = decodeURI("${start_folder}"); popup.window.BUCKET_NAME = decodeURI("${bucket_name}");`)
-}
-
-if (!gotTheLock) {
-  app.quit()
-} else {
-  app.on('second-instance', (event, commandLine) => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-
-      // open the deep link in a new window using s3 browser
-      let deep_link = commandLine.pop().slice(0, -1); // full url with s3:// prefix
-      openDeepLink(mainWindow, deep_link);
+// Only intercept s3 protocol if specified in config
+let config = JSON.parse(fs.readFileSync(path.resolve(__dirname,"../config.json"), "utf-8"));
+if ("intercept_s3_protocol" in config && config.intercept_s3_protocol) {
+  // https://www.electronjs.org/docs/latest/tutorial/launch-app-from-url-in-another-app
+  if (process.defaultApp) {
+    if (process.argv.length >= 2) {
+      app.setAsDefaultProtocolClient(s3_protocol, process.execPath, [path.resolve(process.argv[1])])
     }
-  })
+  } else {
+    app.setAsDefaultProtocolClient(s3_protocol)
+  }
+  const gotTheLock = app.requestSingleInstanceLock()
+
+  function openDeepLink(mainWindow, deep_link) {
+    // remove s3 and the '://'
+    let s3_path = deep_link.slice(s3_protocol.length + "://".length); 
+    // get the first part of the path, which is the bucket name
+    let bucket_name = s3_path.split("/")[0];
+    // get the rest of the path, which is the folder name
+    let start_folder = s3_path.slice(bucket_name.length + 1);
+
+    // open an s3 browser window
+    mainWindow.webContents.executeJavaScript(`try{let popup}catch{}; popup = window.open(window.location.href, "S3 Browser"); popup.window.S3_BROWSER_MODE = "${s3_browser_modes.DEEP_LINK}"; popup.window.START_FOLDER = decodeURI("${start_folder}"); popup.window.BUCKET_NAME = decodeURI("${bucket_name}");`)
+  }
+
+  if (!gotTheLock) {
+    app.quit()
+  } else {
+    app.on('second-instance', (event, commandLine) => {
+      // Someone tried to run a second instance, we should focus our window.
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore()
+        mainWindow.focus()
+
+        // open the deep link in a new window using s3 browser
+        let deep_link = commandLine.pop().slice(0, -1); // full url with s3:// prefix
+        openDeepLink(mainWindow, deep_link);
+      }
+    })
+  }
+} else {
+  // remove as default protocol client
+  if (process.defaultApp) {
+    if (process.argv.length >= 2) {
+      app.removeAsDefaultProtocolClient(s3_protocol, process.execPath, [path.resolve(process.argv[1])])
+    }
+  } else {
+    app.removeAsDefaultProtocolClient(s3_protocol)
+  }
 }
 
 function createWindow() {
