874005cb45500e4b81af317ba7e1253bc0cb6011
TrevorBurgoyne <trevorburgoyne@gmail.com>
2024-06-20 10:53:00 -0500

    Test multiple inputs and make initial styles

--- react-frontend/src/components/Input.js
+++ react-frontend/src/components/Input.js
@@ -8,7 +8,7 @@ class Input extends Component {
 
         this.QASM           = props.QASM;
         this.display_string = props.display_string;
-        this.inputs         = props.inputs;
+        this.inputs         = props.inputs; // Array of input objects, {name: string, type: string, default: string}
 
         // Bind functions
     }
@@ -18,7 +18,22 @@ class Input extends Component {
     render() {
         return (
             <div className="Input">
-                { this.display_string }
+                <div className="input-title">
+                    { this.display_string }
+                </div>
+                <div className="input-fields-container">
+                    {this.inputs.map((input, index) => {
+                        return (
+                            <div key={index} className="input-field">
+                                <label>{input.name}</label>
+                                <input type={input.type} defaultValue={input.default || ""}></input>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="input-submit">
+                    <button>Submit</button>
+                </div>
             </div>
         )
     }
