a6f79ad2ac70e93804fd7f6147c56b5fc5e87239
TrevorBurgoyne <trevorburgoyne@gmail.com>
2024-06-24 19:46:56 -0500

    Fix paginated s3 queries

--- terraform-backend/lambdas/utils/s3_utils.py
+++ terraform-backend/lambdas/utils/s3_utils.py
@@ -41,18 +41,23 @@ def get_all_signed_urls_in_folder(bucket_name, folder_path, s3_client=None):
         s3_client = boto3.client("s3", config=Config(signature_version="s3v4"))
     if not folder_path.endswith("/"):
         folder_path += "/"
-    result = client.list_objects(Bucket=bucket_name, Prefix=folder_path, Delimiter="/")
-    if result.get("Contents"):
-        return {
-            str(Path(str(o.get("Key")).split("/")[-1]).with_suffix("")): # Filename w/o ext
-            get_signed_url(
-                bucket_name, folder_path, str(o.get("Key")).split("/")[-1], s3_client
-            )
-            for o in result.get("Contents") if str(o.get("Key")) != folder_path
-        }
-    else:
+    
+    # use pagination to support > 1000 objects
+    paginator = s3_client.get_paginator('list_objects')
+    operation_parameters = {'Bucket': bucket_name, 'Prefix': folder_path, 'Delimiter': "/"}
+    page_iterator = paginator.paginate(**operation_parameters)
+
+    signed_urls = {}
+    for page in page_iterator:
+        if page.get("Contents"):
+            for o in page.get("Contents"):
+                if str(o.get("Key")) != folder_path:
+                    signed_urls[str(Path(str(o.get("Key")).split("/")[-1]).with_suffix(""))] = get_signed_url(
+                        bucket_name, folder_path, str(o.get("Key")).split("/")[-1], s3_client
+                    )
+    if not signed_urls:
         print(f"No contents found in {folder_path}.")
-        return {}
+    return signed_urls
 
 if __name__ == "__main__":
     res = get_all_signed_urls_in_folder("sentera-rogues-data", "Farmer City 2022/Strip Trial/Planting 1/Videos/6-21/Row 1b, 6a/3840x2160@120fps/Pass A/DS Splits/DS 002/oblique Raw Images/")
